16acf91ad4ffc944966285e42535803396f8084e
Aldair Avelino <aldair03avelino@gmail.com>
2022-04-05 18:04:58 +0100

    Estudo das variáveis

---
+++ variaveis/variaveis.php
@@ -0,0 +1,9 @@
+<?php 
+    $age=18;
+    $username="\nAldair Avelino\n";
+    $country="\nAngola";
+    $state="\nSingle";
+    echo $state;
+    echo $country;
+    echo $username;
+    echo $age;
